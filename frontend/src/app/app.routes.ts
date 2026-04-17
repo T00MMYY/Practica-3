c0da481f61cf3e781fd9dbe55a50bcd054b51657
Tommy <tommyfigjim@campus.monlau.com>
2026-04-17 17:33:14 +0200

    contacto, cambios nav

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import {Catalogo} from './components/catalogo/catalogo';
+import {Contacto} from './components/contacto/contacto'
 
 export const routes: Routes = [
     { path: '', component: Home},
     { path: 'catalogo', component: Catalogo},
+    {path: 'contacto', component: Contacto},
 ];
