016c101607fb9935145210ba9e89e4e7511b36cb
Tommy <tommyfigjim@campus.monlau.com>
2026-04-24 18:06:32 +0200

    login + home

--- frontend/src/app/components/home/home.ts
+++ frontend/src/app/components/home/home.ts
@@ -9,4 +9,11 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  productosDestacados = [
+    { nombre: 'AirStride Pro', categoria: 'Running', precio: 129.99, imagen: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff' },
+    { nombre: 'AirStride Pro', categoria: 'Running', precio: 129.99, imagen: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff' },
+    { nombre: 'AirStride Pro', categoria: 'Running', precio: 129.99, imagen: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff' },
+    { nombre: 'AirStride Pro', categoria: 'Running', precio: 129.99, imagen: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff' }
+  ];
+}
