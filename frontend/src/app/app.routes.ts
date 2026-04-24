b25bb723078e0bb1ee4d0658201e52268c2d2dde
T00MMYYY <tommyfigueroajimenez@gmail.com>
2026-04-24 11:52:39 +0200

    home, login, register y rutas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
-import {Catalogo} from './components/catalogo/catalogo';
-import {Contacto} from './components/contacto/contacto'
+import { Catalogo } from './components/catalogo/catalogo';
+import { Contacto } from './components/contacto/contacto';
+import { Login } from './components/auth/login/login';
+import { Register } from './components/auth/register/register';
 
 export const routes: Routes = [
-    { path: '', component: Home},
-    { path: 'catalogo', component: Catalogo},
-    {path: 'contacto', component: Contacto},
+    { path: '', component: Home },
+    { path: 'catalogo', component: Catalogo },
+    { path: 'contacto', component: Contacto },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register }
 ];
